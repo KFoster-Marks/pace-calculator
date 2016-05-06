2597888a3704bbe913b536aca7719f8125eb4f38
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-05-05 20:42:54 -0700

    refactored js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 "use strict";
 
-function calculatePace(hours, minutes, seconds, distance) {
+function calculatePace() {
 	var distance = document.getElementById("distance").value;
 	var hours = Number(document.getElementById("hours").value);
 	var minutes = Number(document.getElementById("minutes").value);
 	var seconds = Number(document.getElementById("seconds").value);
 
-	var hourMin = (hours * 60);
-  var minutesPlusHoursSeconds = (hourMin + minutes)*60;
-  var secondsTotal = minutesPlusHoursSeconds + seconds;
-  var totalTime = (secondsTotal/60)/distance;
-  var float = totalTime - Math.floor(totalTime);
-  var totalMinutes = Math.floor(totalTime);
+  var pacePlusFloat = (((((hours * 60) + minutes)*60) + seconds)/60)/distance;
+  var float = pacePlusFloat - Math.floor(pacePlusFloat);
+  var minutesIsolated = Math.floor(pacePlusFloat);
   seconds = Math.floor(float*60);
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
-  document.getElementById("paceDisplay").innerHTML = "Your pace was " + totalMinutes + ":" + seconds;
+  document.getElementById("paceDisplay").innerHTML = "Your pace was " + minutesIsolated + ":" + seconds;
 }
